558e40c70f8d6718e60ded4e5cfd84e57d9d7e6e
Konstantin <konst@ntinh.am>
2022-06-16 14:51:25 -0400

    replaced default endpoint with cart

--- customerAPI/index.js
+++ customerAPI/index.js
@@ -1,17 +1,23 @@
 const http = require('http');
+
 const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 8000;
 
-//event listener
-const requestListener = (req, res) {
+// event listener
+const requestListener = ((req, res) => {
+  if (req.url === '/cart') {
     res.writeHead(200);
-    res.end("My first server!");
-};
+    res.end('This is the /cart endpoint');
+  } else {
+    res.end(`{"error": "${http.STATUS_CODES[404]}"}`);
+  }
+});
 
-//create http server
+// create http server
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
+  // eslint-disable-next-line no-console
+  console.log(`Server is running on http://${host}:${port}`);
 });
 
 exports.server = server;
