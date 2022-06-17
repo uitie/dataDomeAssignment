b8316cf362fc6d667697fe9acde1eb33e08dbf6d
Konstantin <konst@ntinh.am>
2022-06-16 23:50:32 -0400

    implemented basic proxy server

--- customerAPI/proxyModule/proxy.js
+++ customerAPI/proxyModule/proxy.js
@@ -1,13 +1,46 @@
+/* eslint-disable no-console */
 const net = require('net');
 
-const port = 0; // OS will assign random unused port
-const host = 'localhost' || '0.0.0.0';
+const proxyPort = 8080; // OS will assign random unused port
+const proxyHost =/*  'localhost' || */ '0.0.0.0';
 const server = net.createServer();
 
-server.on('connection', (socket) => {
+server.on('connection', (clientToProxySocket) => {
   console.log('Connected to proxy');
-  socket.once("data", (data) => {
-    console.log(data.toString());
+
+  clientToProxySocket.once('data', (data) => {
+    const serverPort = 80;
+    // Filter address info from request
+    const serverAddress = data
+      .toString()
+      .split('Host: ')[1]
+      .split('\r\n')[0];
+
+    console.log(serverAddress);
+
+    // Create connection to destination server
+    const proxyToServerSocket = net.createConnection({
+      host: serverAddress,
+      port: serverPort,
+    }, () => {
+      console.log('Proxy to server set up');
+    });
+
+    proxyToServerSocket.write(data);
+
+    clientToProxySocket.pipe(proxyToServerSocket);
+    proxyToServerSocket.pipe(clientToProxySocket);
+
+    proxyToServerSocket.on('error', (err) => {
+      console.log('Proxy to server error');
+      console.log(err);
+    });
+
+    clientToProxySocket.on('error', (err) => {
+      console.log('Client to proxy error');
+      console.log(err);
+    });
+  });
 });
 
 server.on('error', (err) => {
@@ -19,6 +52,6 @@ server.on('close', () => {
   console.log('Client disconnected');
 });
 
-server.listen(port, host, () => {
-  console.log(`Proxy server listening at http://${host}:${server.address().port}`);
+server.listen(proxyPort, proxyHost, () => {
+  console.log(`Proxy server listening at http://${proxyHost}:${/* server.address(). */proxyPort}`);
 });
